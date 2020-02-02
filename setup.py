019d58e84ad14451bfd331454c65e52f0f8eb640
Senthil Kumar <admin@amptorque.com>
2020-02-02 13:06:20 +0100

    Update for part 3 topics

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="future-scientist",
+    version="0.0.1",
+    author="Senthil",
+    author_email="senthil@example.com",
+    description="teaching purpose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/senthilskm983/future-scientist",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNUv3 ",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'requests',
+      ],
+    python_requires='>=3.6',
+)
